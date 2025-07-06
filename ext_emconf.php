5eeb20ee386f0f7b7d352dd959f9bb39e2d23012
mirco <klein@ganzgraph.de>
2025-07-06 18:50:53 +0200

    add ext_emconf

---
+++ ext_emconf.php
@@ -0,0 +1,16 @@
+<?php
+
+$EMCONF[$_EXTKEY] = [
+    'title' => 'Simple Shop',
+    'description' => 'A simple shopping cart extension.',
+    'category' => 'plugin',
+    'author' => 'Mirco Klein',
+    'author_email' => 'mirco.klein@icloud.com',
+    'state' => 'beta',
+    'version' => '1.0.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '12.4.0-13.9.99',
+        ],
+    ],
+];
