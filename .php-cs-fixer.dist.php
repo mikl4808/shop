12f79c05f18135858e83b9e52bd336f9ad355f46
mirco <klein@ganzgraph.de>
2025-07-06 20:07:31 +0200

    add php-cs-fixer config

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,16 @@
+<?php
+
+$finder = (new PhpCsFixer\Finder())
+    ->in(__DIR__ . '/Classes')
+    ->in(__DIR__ . '/Tests')
+;
+
+return (new PhpCsFixer\Config()->setUnsupportedPhpVersionAllowed(true))
+    ->setRules([
+        '@PSR12' => true,
+        'strict_param' => true,
+        'array_syntax' => ['syntax' => 'short'],
+    ])
+    ->setFinder($finder)
+    ->setRiskyAllowed(true)
+;
